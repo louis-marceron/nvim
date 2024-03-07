7f0ba1d238f56a24c0d77708410461d7c1a0edc3
Louis Marceron <louis.marceron@protonmail.com>
2024-03-07 16:13:42 +0100

    add harpoon

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -41,8 +41,8 @@ require('lazy').setup {
   require 'kickstart/plugins/autopairs',
   -- require 'kickstart.plugins.filetree',
   require 'kickstart.plugins.indent_line',
-
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.harpoon',
+  require 'kickstart.plugins.debug',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
