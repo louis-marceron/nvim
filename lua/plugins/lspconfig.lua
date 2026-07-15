9457215cdd0d86c0e17bf5dec30834b70d9bac0c
Louis Marceron <louis.marceron@coexya.eu>
2026-07-15 15:14:51 +0200

    asm lsp support

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -205,6 +205,7 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        asm_lsp = {},
         jdtls = {},
         dockerls = {},
         docker_compose_language_service = {},
