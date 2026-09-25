9a7325d8becbd3a1128d68da201fa781a4ffe81b
Louis Marceron <louis.marceron@coexya.eu>
2026-09-25 17:08:58 +0200

    fix comments in big files

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,3 +5,11 @@ require('snacks').setup {
   bigfile = {},
   input = {},
 }
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'bigfile',
+  callback = function(event)
+    local filetype = vim.filetype.match { buf = event.buf } or 'text'
+    vim.bo[event.buf].commentstring = vim.filetype.get_option(filetype, 'commentstring')
+  end,
+})
